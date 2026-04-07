4126517466eb8ca46645e03230b3cad0910d416f
sanguogege <sanguogege@outlook.com>
2026-04-07 20:42:34 +0800

    v1.4 check

--- wxt.config.ts
+++ wxt.config.ts
@@ -11,13 +11,5 @@ export default defineConfig({
   manifest: {
     permissions: ['storage', 'tabs', 'activeTab', "scripting",],
     host_permissions: ['*://*.bilibili.com/*'],
-    browser_specific_settings: {
-      gecko: {
-        // 这里的 ID 格式必须是：名称@域名 或 像 UUID 
-        id: 'bilibili-movie-control@sanguogege.com ',
-        // @ts-ignore
-        data_collection_permissions: false, // 如果不收集数据设为 false
-      },
-    },
   },
 });
